4754646ec9f50d3a4e0b208bebcd4289148a5aed
Vicky <vicky@example.com>
2025-02-25 14:46:29 +0530

    update

---
+++ 06_ip_address/04_parse_interface_ip_address.py
@@ -0,0 +1,90 @@
+# using regular expression to matching interface details, Then finally print list inside a dictionary.
+
+import sys
+import time
+import socket
+import datetime
+import os
+import ipaddress
+import re
+from paramiko import client, ssh_exception  # type: ignore
+from getpass import getpass
+from pprint import pprint
+
+
+int_pattern = re.compile(r'(\S+)\s+(([\d\.]+)|unassigned)\s+((protocol-up|protocol-down)(\/)(link-up|link-down))\/(\S+)')
+
+
+username = input("Enter Username:")
+
+if not username:
+    username = 'admin'
+    print(f"No username provided, considering default username {username}")
+
+password = getpass(f"\U0001F511 Enter password of the user {username}: ") or "Admin_1234!"
+
+new_cmd = ["show ip interface brief"]
+ip_subnet = ipaddress.IPv4Network(input("Enter subnet to match: "))
+
+os.chdir('06_ip_address/backup')
+def cisco_cmd_exicuter(hostname, commands):
+    try:
+        print(f"connecting to the device {hostname}..... ")
+        now = datetime.datetime.now().replace(microsecond=0)
+        now_12hr = now.strftime("%d-%m-%Y %I:%M:%S %p")   
+        current_conf_file = f"{now_12hr}_{hostname}.txt"  
+        ssh_client = client.SSHClient()
+        ssh_client.set_missing_host_key_policy(client.AutoAddPolicy())
+        ssh_client.connect(hostname=hostname,
+                           username=username,
+                           password=password,
+                           look_for_keys=False, allow_agent=False)
+
+        print(f"connected to the device\t{hostname} ")
+
+        device_access = ssh_client.invoke_shell()
+        device_access.send("terminal len 0\n")
+        with open(current_conf_file, 'w') as cmd_data: 
+            for cmd in commands:
+                device_access.send(f"{cmd}\n") #---> i add \n . because that is Enter, im using list in cmd.
+                time.sleep(2)
+                output = device_access.recv(65535).decode() #--> i put the decode here because first read for output in parsing purpose.
+                cmd_data.write(output) 
+                print(output)
+
+        ssh_client.close()
+
+        print('\n######## Parse Formate ############')  
+        
+        int_iter = int_pattern.finditer(output)
+        int_list =list()
+        for intf in int_iter:
+            int_dict = dict()
+            int_dict['Interface_name'] = intf.group(1)
+            int_dict['IP_address'] = intf.group(2)
+            int_dict['Staus'] = intf.group(4)
+            int_list.append(int_dict)
+        pprint(int_list)
+        
+        print(f'\n######## Interface part of subnet{ip_subnet}  ############')  
+
+        for intf in int_list:
+            try:
+                ip = ipaddress.IPv4Address(intf['IP_address'])
+                if ip in ip_subnet:
+                    print(intf['Interface_name'],ip)
+            except ValueError:
+                continue
+
+    except ssh_exception.AuthenticationException:
+        print("\U00002757\U00002757\U00002757Authentication failed, Check your credentials \U00002757\U00002757\U00002757")
+    except socket.gaierror:
+        print("\U00002757\U00002757\U00002757Check the hostname \U00002757\U00002757\U00002757")
+    except ssh_exception.NoValidConnectionsError:
+        print("\U00002757\U00002757\U00002757SSH Port not reachable\U00002757\U00002757\U00002757")
+    except:
+        print("\U00002757\U00002757\U00002757Exception Occured \U00002757\U00002757\U00002757")
+        print(sys.exc_info())
+
+cisco_cmd_exicuter('sbx-nxos-mgmt.cisco.com', new_cmd)
+
