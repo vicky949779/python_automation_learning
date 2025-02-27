49722c38d6d3fd875d1670e614b7a89ccce0982d
Vicky <vicky@example.com>
2025-02-27 23:37:39 +0530

    update

--- 08_modules_packages/testfn_frmdiffdir.py
+++ 08_modules_packages/testfn_frmdiffdir.py
@@ -1,4 +1,12 @@
 import sys
 
+''' ### output is print liside of list ### '''
+# sys.path.append('07_functions') # it will add your coustem python file to as package directory
+sys.path.insert(1,'07_functions') # This will insert the directory into the package with index position
 print(sys.path) # these are the directory Pyhton interpreters reading for the modules and packages. below the package details
 # ['/home/vicky/python_automation_learning/08_modules_packages', '/usr/lib/python310.zip', '/usr/lib/python3.10', '/usr/lib/python3.10/lib-dynload', '/home/vicky/py_venv/py_automation_env/lib/python3.10/site-packages']
+
+''' now you add your local package '''
+from password_gen import user_cmd_gen # type: ignore
+
+print(user_cmd_gen("just_try",100))
